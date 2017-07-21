b1eddcebd3b7114db9df933d1738db587221ec84
Yuri <yurijrdn@gmail.com>
2017-07-21 12:46:52 -0300

    Relação de informaçõesvindas do facebook com R.

--- src/main/webapp/resources/R scripts/pds_posts_comments.R
+++ src/main/webapp/resources/R scripts/pds_posts_comments.R
@@ -1,8 +1,8 @@
  get_fb_dataset <- function(token, page){
        
-       # token <- 'EAACEdEose0cBAOrofqx6QpCmkp6CKBiets8cFAJ3O946dZCULfZCmWrGSM8XrYeA12I7FGxMZAUD6eDQUkazXbmLJTKRZCBeE5gGTIphRGf29ppq0N9GUpU2pKVbanbkZCZAq1NLlTa8afZACZBUfhQ5HmeTBBYcbf7YmZBuPZBwZC9Irw1JEX7ZBBiGZCIVB2ZBMyqnEZD'
+       # token <- 'EAACEdEose0cBAGJxhM7AGHW2CSeyAHw0E0C7BCtF5fZAHqqpSnwOzx2xgrI38XLDnDs8s5XJe0KEveJS11AVSTkNYfsvok9kXgTACOPiLwGNndyJzUZABWtPWZCBbQ7cxW2skId8YOze1rjpjWER0RCZAbGBZAwkrHty68YQXfxcQi1ITWByjAZAqu36tmOPcZD'
        # page <- 'SKYbrasil'
-       
+       # 
   require(Rfacebook)
             # data <- as.POSIXct('2016/07/12', tz = 'UTC')
        posts <- getPage(page = page, token = token, reactions = T)
@@ -14,17 +14,20 @@
   
  }
  
+ 
  get_all_comments <- function(posts, token){
       
-       novo <- getPost(post = posts$id[1], token = token, likes = F)
-       novo <- novo$comments
+       lista <- getPost(post = posts$id[1], token = token, likes = F)
+       novo <- data.frame(lista$post, lista$comments, check.names = T)
        
        for(i in 2:nrow(posts)){
+             
              if(!is.na(posts$id[i])){
-                   aux <- getPost(post = posts$id[i], token = token, likes = F)
                    
+                   aux <- getPost(post = posts$id[i], token = token, likes = F)
+                   aux <- data.frame(aux$post, aux$comments, check.names = T)
                    # aux <- ldply(aux, data.frame)
-                   novo <- merge(novo, aux$comments, all = T)
+                   novo <- merge(aux, novo, all = T)
              }
        }
        return(novo)
